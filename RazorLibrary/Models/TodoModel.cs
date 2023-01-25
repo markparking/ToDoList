9c161619b8c31d0bd61c707b6aeb20965f0049b3
Mark-PC <Mark-PC@DESKTOP-FBC0J6Q>
2023-01-25 11:28:14 +0100

    2

---
+++ RazorLibrary/Models/TodoModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RazorLibrary.Models;
+
+public class TodoModel
+{
+    public string TodoItem { get; set; }
+    public bool IsComplete { get; set; }
+}
